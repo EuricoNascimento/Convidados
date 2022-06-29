98e88f3052cc9e2b771fc88ce3fc95e30a96f312
Eurico Neto <57076993+EuricoNascimento@users.noreply.github.com>
2022-06-29 14:08:33 -0300

    Initial commit

---
+++ app/src/main/java/com/euriconfneto/convidados/ui/gallery/PresentViewModel.kt
@@ -0,0 +1,13 @@
+package com.euriconfneto.convidados.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PresentViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
