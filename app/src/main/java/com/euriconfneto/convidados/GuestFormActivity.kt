7fe538da150ae1aa64a12ce4723f869ce53d943c
Eurico Neto <57076993+EuricoNascimento@users.noreply.github.com>
2022-06-29 23:05:09 -0300

    Implementação do evento de Click

--- app/src/main/java/com/euriconfneto/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/euriconfneto/convidados/GuestFormActivity.kt
@@ -2,10 +2,24 @@ package com.euriconfneto.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.euriconfneto.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+
+        }
     }
 }
